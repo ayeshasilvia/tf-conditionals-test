2790a824796191d68ef55656bcd1bcae29cab66e
Ayesha Mosaddeque <ayesha.mosaddeque@intelematics.com>
2018-08-30 15:48:15 +1000

    removing few oh effs.

--- application/variables.tf
+++ application/variables.tf
@@ -1,12 +1,10 @@
 
 # Conditionals
 variable "spinnaker_managed" {
-  description = "Flag that decides if the VM creation is managed by Spinnaker or Terraform"
   default     = false
 }
 
 variable "on_prem" {
-  description = "Flag that decides if Audit service is deployed on-prem or on cloud"
   default     = false
 }
 
